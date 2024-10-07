e70ad4c6388e10c49b0838c008ab1bef7723ed1f
lpeterson52 <111457379+lpeterson52@users.noreply.github.com>
2024-10-07 00:20:48 -0700

    Initial Commit

---
+++ DoorMate/DoorMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoorMateApp.swift
+//  DoorMate
+//
+//  Created by Lucas Peterson on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct DoorMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
